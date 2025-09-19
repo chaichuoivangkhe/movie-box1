ab20ff134e5de9f69e0685983f7981cf6d3d3cbc
2200000453@nttu.edu.vn <2200000453@nttu.edu.vn>
2025-09-19 21:22:34 +0700

    fix

--- movie/src/components/HeroSection.jsx
+++ movie/src/components/HeroSection.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import bgImage from '../assets/backgroundImage.png'
 import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
 import { useNavigate } from 'react-router-dom';
 const HeroSection = () => {
     const navigate = useNavigate();
     return (
-        <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("backgroundImage.png")] bg-cover bg-center h-screen'>
+        <div
+            className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center bg-no-repeat h-screen'
+            style={{ backgroundImage: `url(${bgImage})` }}
+        >
             <img src={assets.disneyLogo} alt="" className="max-h-25 lg:h-30 mt-20" />
             <h1 className='text-5xl md:text-[100px] md:leading-18 font-semibold max-w-200 drop-shadow-sm'>Lilo & Stitch</h1>
             <div className='flex items-center gap-4 text-white-300'>
